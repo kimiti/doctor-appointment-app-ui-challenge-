4cff5eb69588703478f35273477ecc198dff0a1b
kimiti <kimitikevin@gmail.com>
2025-03-02 08:00:00 +0300

    update intro screen

--- app/src/main/java/com/example/doctorappointmentapplication/feature/intro/IntroScreen.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/intro/IntroScreen.kt
@@ -1,2 +1,51 @@
 package com.example.doctorappointmentapplication.feature.intro
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.doctorappointmentapplication.R
+
+@Composable
+fun IntroScreen(onStartClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.purple))
+    ) {
+        Image(
+            painter = painterResource(R.drawable.white_halo),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Image(
+            painter = painterResource(R.drawable.intro_women),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+
+    }
+}
+
+
+@Preview
+@Composable
+fun IntroScreenPreview() {
+    IntroScreen(onStartClick = {})
+}
