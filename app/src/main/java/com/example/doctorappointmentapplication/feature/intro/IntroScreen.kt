1b51bedd13696b0e2b6886a89bc5396731697205
kimiti <kimitikevin@gmail.com>
2025-03-04 08:10:00 +0300

    update intro screen

--- app/src/main/java/com/example/doctorappointmentapplication/feature/intro/IntroScreen.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/intro/IntroScreen.kt
@@ -3,16 +3,34 @@ package com.example.doctorappointmentapplication.feature.intro
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.doctorappointmentapplication.R
 
 @Composable
@@ -39,6 +57,48 @@ fun IntroScreen(onStartClick: () -> Unit) {
                 .fillMaxWidth(),
             contentScale = ContentScale.FillWidth
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .statusBarsPadding()
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+        ) {
+            Text(
+                text = stringResource( R.string.sub_intro),
+                color = Color.White,
+                fontSize = 34.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = onStartClick,
+                modifier = Modifier.size(width = 170.dp, height = 60.dp),
+                shape = RoundedCornerShape(100.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(R.color.darkPurple),
+                    contentColor = Color.White
+                ),
+                contentPadding = PaddingValues(start = 8.dp, end = 16.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = stringResource(R.string.get_started),
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(
+                        painter = painterResource(R.drawable.white_arrow),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
+                }
+
+            }
+        }
+
 
     }
 }
