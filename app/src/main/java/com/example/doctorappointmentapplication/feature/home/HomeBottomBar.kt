112fabd4e46cb6aff8f71b36f98c2990ae545a46
kimiti <kimitikevin@gmail.com>
2025-03-05 08:20:00 +0300

    adds bottom navigation bar

--- app/src/main/java/com/example/doctorappointmentapplication/feature/home/HomeBottomBar.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/home/HomeBottomBar.kt
@@ -1,7 +1,64 @@
 package com.example.doctorappointmentapplication.feature.home
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.doctorappointmentapplication.R
 
 @Composable
-fun HomeBottomBar() {
+fun HomeBottomBar(selected: Int, onSelect: (Int) -> Unit) {
+    NavigationBar(
+        containerColor = colorResource(R.color.lightGray),
+        tonalElevation = 1.dp,
+        modifier = Modifier.height(68.dp),
+        windowInsets = WindowInsets(0)
+    ) {
+        val titles = listOf("Explorer", "Wishlist", "Settings", "Account")
+
+        val icons = listOf(
+            R.drawable.btn_1,
+            R.drawable.btn_2,
+            R.drawable.btn_3,
+            R.drawable.btn_4,
+        )
+
+        titles.forEachIndexed { index, string ->
+            NavigationBarItem(
+                selected = selected == index,
+                onClick = {onSelect(index)},
+                icon = {
+                    Icon(
+                        painter = painterResource(icons[index]),
+                        contentDescription = null,
+                        modifier = Modifier.size(22.dp),
+                        tint = Color.Unspecified
+                    )
+                },
+                label = {
+                    Text(text = string, fontSize = 18.sp, color = Color.Black)
+                },
+                alwaysShowLabel = true
+
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun HomeBottomBarPreview(){
+    HomeBottomBar(selected = 0, onSelect = {})
 }
