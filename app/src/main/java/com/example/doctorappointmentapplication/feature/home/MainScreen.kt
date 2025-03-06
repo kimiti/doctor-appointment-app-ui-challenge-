137e92d04be630144fed8f67d66bd5816047963a
kimiti <kimitikevin@gmail.com>
2025-03-06 08:20:00 +0300

    adds home header

--- app/src/main/java/com/example/doctorappointmentapplication/feature/home/MainScreen.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/home/MainScreen.kt
@@ -1,4 +1,35 @@
 package com.example.doctorappointmentapplication.feature.home
 
-class MainScreen {
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+    var selectedBottom by remember { mutableStateOf(0) }
+
+
+    Scaffold(
+        containerColor = Color.White,
+        bottomBar = {
+            HomeBottomBar(
+                selected = selectedBottom,
+                onSelect = { selectedBottom = it }
+            )
+        }
+    ) { inner ->
+        LazyColumn(contentPadding = inner) { }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
