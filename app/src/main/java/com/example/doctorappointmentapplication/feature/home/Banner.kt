d2c638e74f8181b44c2617e13fe63b90c99d2b5f
kimiti <kimitikevin@gmail.com>
2025-03-09 08:00:00 +0300

    update banner file

--- app/src/main/java/com/example/doctorappointmentapplication/feature/home/Banner.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/home/Banner.kt
@@ -1,8 +1,31 @@
 package com.example.doctorappointmentapplication.feature.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.doctorappointmentapplication.R
 
 @Composable
 fun Banner(){
-
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+            .padding(top = 24.dp),
+        contentAlignment = Alignment.BottomCenter
+    ){
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillBounds
+        )
+    }
 }
