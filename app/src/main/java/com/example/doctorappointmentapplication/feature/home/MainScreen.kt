1c637c8803f54628ae121bf59ca51a00da20ec42
kimiti <kimitikevin@gmail.com>
2025-03-10 08:30:00 +0300

    update main screen

--- app/src/main/java/com/example/doctorappointmentapplication/feature/home/MainScreen.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/home/MainScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun MainScreen() {
     var selectedBottom by remember { mutableStateOf(0) }
-    
+
     Scaffold(
         containerColor = Color.White,
         bottomBar = {
