2d4da4d49be39df3e10ea80a95de1e988a3fd595
kimiti <kimitikevin@gmail.com>
2025-03-11 08:40:00 +0300

    adds repo and viewmodel

--- app/src/main/java/com/example/doctorappointmentapplication/core/model/CategoryModel.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/core/model/CategoryModel.kt
@@ -1,3 +1,7 @@
 package com.example.doctorappointmentapplication.core.model
 
-data class CategoryModel()
+data class CategoryModel(
+    val Id:Int = 0,
+    val ame: String = "",
+    val picture: String = ""
+)
