e0b9336065e5af3d50f97d445226d23f7c33faa4
kimiti <kimitikevin@gmail.com>
2025-03-12 08:10:00 +0300

    adds repo and viewmodel

--- app/src/main/java/com/example/doctorappointmentapplication/feature/home/CategoryItem.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/home/CategoryItem.kt
@@ -1,2 +1,18 @@
 package com.example.doctorappointmentapplication.feature.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.doctorappointmentapplication.core.model.CategoryModel
+
+@Composable
+fun CategoryItem(item: CategoryModel){
+
+}
+
+
+@Preview
+@Composable
+fun CategoryItemPreview(){
+    val item = CategoryModel(0, "Category 1", "picture_url")
+    CategoryItem(item = item)
+}
