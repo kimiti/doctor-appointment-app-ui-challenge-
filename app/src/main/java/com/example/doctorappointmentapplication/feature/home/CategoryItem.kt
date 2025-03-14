8ee6c7605a78aa6deaa3e044a6ea3bf0eb4730a5
kimiti <kimitikevin@gmail.com>
2025-03-14 08:00:00 +0300

    update category item component

--- app/src/main/java/com/example/doctorappointmentapplication/feature/home/CategoryItem.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/feature/home/CategoryItem.kt
@@ -1,21 +1,30 @@
 package com.example.doctorappointmentapplication.feature.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -24,23 +33,28 @@ import com.example.doctorappointmentapplication.R
 import com.example.doctorappointmentapplication.core.model.CategoryModel
 
 @Composable
-fun CategoryItem(item: CategoryModel){
-    Column(modifier = Modifier
-        .wrapContentSize()
-        .padding(horizontal = 8.dp),
+fun CategoryItem(item: CategoryModel) {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(horizontal = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Box(modifier = Modifier
-            .size(70.dp)
-            .clip(CircleShape)
-            .background(colorResource(R.color.lightPurple)),
+    ) {
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .clip(CircleShape)
+                .background(colorResource(R.color.lightPurple)),
             contentAlignment = Alignment.Center
         ) {
-            AsyncImage(
-                model = item.picture,
-                contentDescription = null,
-                modifier = Modifier.size(30.dp),
-                contentScale = ContentScale.Fit
+            Image(
+                painter = painterResource(
+                    context.resources.getIdentifier(
+                        item.picture, "drawable", context.packageName
+                    )
+                ),
+                contentDescription = null
             )
         }
         Spacer(Modifier.height(8.dp))
@@ -53,14 +67,31 @@ fun CategoryItem(item: CategoryModel){
 }
 
 @Composable
-fun CategoryRow(){
-    
+fun CategoryRow(items: List<CategoryModel>) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 100.dp)
+    ) {
+        if (items.isEmpty()) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        } else {
+            LazyRow(
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                items(items) { item ->
+                    CategoryItem(item)
+                }
+            }
+        }
+    }
 }
 
 
 @Preview
 @Composable
-fun CategoryItemPreview(){
-    val item = CategoryModel(0, "Category 1", "picture_url")
+fun CategoryItemPreview() {
+    val item = CategoryModel(0, "Cardiology", "cardiology")
     CategoryItem(item = item)
 }
