e657456b31aa5218fe79ca0dded5a0ff00631ee7
kimiti <kimitikevin@gmail.com>
2025-03-14 08:20:00 +0300

    update repo component

--- app/src/main/java/com/example/doctorappointmentapplication/core/repository/MainRepository.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/core/repository/MainRepository.kt
@@ -7,12 +7,12 @@ class MainRepository {
         CategoryModel(
             0,
             "Cardiology",
-            "https://firebasestorage.googleapis.com/v0/b/project198-ee047.appspot.com/o/Cardiology.png?alt=media&token=0aa6ee72-1f35-4b20-a00f-2cd72d3e8929"
+            "cardiology"
         ),
         CategoryModel(
             1,
             "Dentistry",
-            "https://firebasestorage.googleapis.com/v0/b/project198-ee047.appspot.com/o/Dentistry.png?alt=media&token=a0206a44-233c-4f94-a0d1-6fa7a7efa679"
+            "dentistry"
         ),
     )
 }
