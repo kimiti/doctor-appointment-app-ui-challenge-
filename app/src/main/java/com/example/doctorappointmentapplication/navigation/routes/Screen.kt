24b43e242fedbfb652ffb041115c665d425b8bc4
kimiti <kimitikevin@gmail.com>
2025-03-16 08:00:00 +0300

    add nav routes

--- app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/Screen.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/Screen.kt
@@ -2,4 +2,7 @@ package com.example.doctorappointmentapplication.navigation.routes
 
 sealed class Screen(val route: String) {
     data object Intro: Screen("intro")
+    data object Home: Screen("home")
+    data object TopDoctor: Screen("topDoctor")
+    data object Detail: Screen("detail")
 }
