842988657002b6497167b8403e081c25aa3b949e
kimiti <kimitikevin@gmail.com>
2025-03-17 08:10:00 +0300

    add nav routes

--- app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/IntroRoutes.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/IntroRoutes.kt
@@ -1,7 +1,11 @@
 package com.example.doctorappointmentapplication.navigation.routes
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.doctorappointmentapplication.feature.intro.IntroScreen
 
-fun NavGraphBuilder.IntroRoutes(onStart: () -> Unit){
-
+fun NavGraphBuilder.IntroRoutes(onStart: () -> Unit) {
+    composable(Screen.Intro.route) {
+        IntroScreen(onStartClick = onStart)
+    }
 }
