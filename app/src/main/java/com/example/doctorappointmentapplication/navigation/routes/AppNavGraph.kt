3b73651e9dcad11dff7d8b515ab4cf901ebb2ad3
kimiti <kimitikevin@gmail.com>
2025-03-18 08:20:00 +0300

    add nav graph

--- app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/AppNavGraph.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/AppNavGraph.kt
@@ -1,8 +1,19 @@
 package com.example.doctorappointmentapplication.navigation.routes
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
-fun AppNavGraph(){
-    
+fun AppNavGraph(
+    nav: NavHostController
+) {
+    NavHost(navController = nav, startDestination = Screen.Intro.route) {
+        IntroRoutes(onStart = {
+            nav.navigate(Screen.Home.route) {
+                popUpTo(Screen.Intro.route) { inclusive = true }
+            }
+        })
+    }
+
 }
