0d7c71671194de801f410bff7550e3745179cbd0
kimiti <kimitikevin@gmail.com>
2025-03-19 08:40:00 +0300

    add home route

--- app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/HomeRoute.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/HomeRoute.kt
@@ -1,2 +1,14 @@
 package com.example.doctorappointmentapplication.navigation.routes
 
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.doctorappointmentapplication.core.viewmodel.MainViewModel
+import com.example.doctorappointmentapplication.feature.home.MainScreen
+
+fun NavGraphBuilder.homeRoute(
+    mainViewModel: MainViewModel
+) {
+    composable(Screen.Home.route) {
+        MainScreen(items = mainViewModel.loadCategories())
+    }
+}
