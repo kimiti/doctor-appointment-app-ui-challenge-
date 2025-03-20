2f05f185a799e5057e9872e50aec4af0b8919b89
kimiti <kimitikevin@gmail.com>
2025-03-20 08:00:00 +0300

    add home route

--- app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/AppNavGraph.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/AppNavGraph.kt
@@ -3,10 +3,12 @@ package com.example.doctorappointmentapplication.navigation.routes
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.example.doctorappointmentapplication.core.viewmodel.MainViewModel
 
 @Composable
 fun AppNavGraph(
-    nav: NavHostController
+    nav: NavHostController,
+    mainViewModel: MainViewModel
 ) {
     NavHost(navController = nav, startDestination = Screen.Intro.route) {
         IntroRoutes(onStart = {
@@ -14,6 +16,8 @@ fun AppNavGraph(
                 popUpTo(Screen.Intro.route) { inclusive = true }
             }
         })
+
+        homeRoute(mainViewModel = mainViewModel)
     }
 
 }
