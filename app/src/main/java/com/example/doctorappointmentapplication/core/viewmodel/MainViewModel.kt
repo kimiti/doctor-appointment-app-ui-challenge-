928fde01930eaace1b7954ecae2ea4360e573080
kimiti <kimitikevin@gmail.com>
2025-03-21 08:10:00 +0300

    update viewmodel

--- app/src/main/java/com/example/doctorappointmentapplication/core/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/core/viewmodel/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.example.doctorappointmentapplication.core.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.doctorappointmentapplication.core.repository.MainRepository
 
-class MainViewModel(val repository: MainRepository): ViewModel() {
+class MainViewModel(
+    val repository: MainRepository = MainRepository()
+) : ViewModel() {
     fun loadCategories() = repository.categoryItems
 }
