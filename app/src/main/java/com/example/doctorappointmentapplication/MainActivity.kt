91e840449bd3e3177e32e204b28b99f6a50611f3
kimiti <kimitikevin@gmail.com>
2025-03-21 08:20:00 +0300

    update viewmodel

--- app/src/main/java/com/example/doctorappointmentapplication/MainActivity.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/MainActivity.kt
@@ -6,30 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.doctorappointmentapplication.core.viewmodel.MainViewModel
-import com.example.doctorappointmentapplication.feature.home.MainScreen
 import com.example.doctorappointmentapplication.navigation.routes.AppNavGraph
 import com.example.doctorappointmentapplication.ui.theme.DoctorAppointmentApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    val mainViewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val mainViewModel = viewModel(modelClass = MainViewModel::class.java)
             val navController = rememberNavController()
             DoctorAppointmentApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     AppNavGraph(navController, mainViewModel = mainViewModel)
-//                    MainScreen(items = mainViewModel.loadCategories())
                 }
             }
         }
