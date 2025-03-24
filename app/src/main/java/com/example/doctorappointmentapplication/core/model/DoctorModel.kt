17d3c28a861d873ff7a6832ac1700d53b40dd997
kimiti <kimitikevin@gmail.com>
2025-03-24 08:00:00 +0300

    add doctor section

--- app/src/main/java/com/example/doctorappointmentapplication/core/model/DoctorModel.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/core/model/DoctorModel.kt
@@ -1,9 +1,16 @@
 package com.example.doctorappointmentapplication.core.model
 
 data class DoctorModel(
+    val address: String = "",
+    val biography: String = "",
+    val id: String = "",
     val name: String = "",
     val special: String = "",
     val rating: Double = 0.0,
+    val location: String = "",
+    val mobile: String = "",
+    val patients: String = "",
+    val site: String = "",
     val picture: String = "",
     val experience: Int = 0
 )
