340919921928333f0cd404948915b26913cc4afe
kimiti <kimitikevin@gmail.com>
2025-03-25 08:10:00 +0300

    add doctor section

--- app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/AppNavGraph.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/navigation/routes/AppNavGraph.kt
@@ -1,6 +1,7 @@
 package com.example.doctorappointmentapplication.navigation.routes
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.doctorappointmentapplication.core.viewmodel.MainViewModel
@@ -8,9 +9,11 @@ import com.example.doctorappointmentapplication.core.viewmodel.MainViewModel
 @Composable
 fun AppNavGraph(
     nav: NavHostController,
-    mainViewModel: MainViewModel
+    mainViewModel: MainViewModel,
+    modifier: Modifier = Modifier
 ) {
-    NavHost(navController = nav, startDestination = Screen.Intro.route) {
+    NavHost(navController = nav, startDestination = Screen.Intro.route,
+        modifier = modifier) {
         IntroRoutes(onStart = {
             nav.navigate(Screen.Home.route) {
                 popUpTo(Screen.Intro.route) { inclusive = true }
