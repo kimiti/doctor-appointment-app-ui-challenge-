8e7841686b77f762ce57eab68c503fc8bc1b2eec
kimiti <kimitikevin@gmail.com>
2025-03-26 08:10:00 +0300

    add doctor section

--- app/src/main/java/com/example/doctorappointmentapplication/core/repository/MainRepository.kt
+++ app/src/main/java/com/example/doctorappointmentapplication/core/repository/MainRepository.kt
@@ -1,6 +1,7 @@
 package com.example.doctorappointmentapplication.core.repository
 
 import com.example.doctorappointmentapplication.core.model.CategoryModel
+import com.example.doctorappointmentapplication.core.model.DoctorModel
 
 class MainRepository {
     val categoryItems = mutableListOf(
@@ -14,5 +15,14 @@ class MainRepository {
             "Dentistry",
             "dentistry"
         ),
+    )
+
+    val doctors = mutableListOf(
+        DoctorModel(
+            name = "Dr. John Doe",
+            special = "Cardiology",
+            rating = 4.5,
+            picture = "dr_jessica_wyne"
+        )
     )
 }
